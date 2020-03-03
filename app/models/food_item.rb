add4d67e307319b0c76150e5ae7979e853d970fd
Cameron Melville <cam.t.melville@gmail.com>
2020-03-03 16:44:55 +1100

    updated validations for each model -- need to run db:migrate

--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -2,11 +2,13 @@ class FoodItem < ApplicationRecord
   MEASURES = ['Grams', 'Kilograms', 'Servings', 'Millilitres', 'Litres']
   belongs_to :business
   belongs_to :food_donation, optional: true
+  has_many :food_item_tags
+  has_many :tags, through: :food_item_tags
 
   validates :name, presence: true
   validates :expiry_date, presence: true
   validates :description, presence: true
-  validates :quatity, presence: true
+  validates :quantity, presence: true
   validates :measure, presence: true
   validates :measure, :inclusion { in: MEASURES }
 end
