da8781ca2813b53b35757169c140851e6a964ee5
Katie King <katharinemargaretking@gmail.com>
2020-03-03 13:12:30 +1100

    charities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_03_005438) do
+ActiveRecord::Schema.define(version: 2020_03_03_013538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,20 @@ ActiveRecord::Schema.define(version: 2020_03_03_005438) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "charities", force: :cascade do |t|
+    t.string "name"
+    t.text "bio"
+    t.string "address"
+    t.string "email"
+    t.string "website"
+    t.float "latitude"
+    t.float "longitude"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_charities_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -53,4 +67,5 @@ ActiveRecord::Schema.define(version: 2020_03_03_005438) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "charities", "users"
 end
