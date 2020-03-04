d8859ebbdb4b1a70ff3c61d5aab0768c69cd1470
Dani P <daniela.s.paino@gmail.com>
2020-03-04 18:07:31 +1100

    claim review added stars

--- app/controllers/food_item_tags_controller.rb
+++ app/controllers/food_item_tags_controller.rb
@@ -1,17 +1,13 @@
 class FoodItemTagsController < ApplicationController
   def create
     @food_item_tag = FoodItemTag.new
-    @food_item_tag.donation = @food_donation
+    @food_item_tag.tag = Tag.find(params[:tag_id]) # ??? will need to check with form
+    # displayed tags label method tag name, submit method id.
     @food_item_tag.food_item = @food_item
   end
 
   def destroy
+    @food_item_tag = FoodItemTag.find(params[:id])
     @food_item_tag.destroy
-  end
-
-  private
-
-  def set_food_item
-    # @food_item = FoodItem.find()
   end
 end
