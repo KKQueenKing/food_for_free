780b4183372e5e584eb863d6223d0014e1ffce89
Dani P <daniela.s.paino@gmail.com>
2020-03-04 12:47:23 +1100

    charity view and controller

--- app/controllers/charities_controller.rb
+++ app/controllers/charities_controller.rb
@@ -9,27 +9,39 @@ class CharitiesController < ApplicationController
 
   def new
     @charity = Charity.new
+
   end
 
   def create
-    @charity = Charity.new
-    #
-    @charity.save!
+    @charity = Charity.new(charity_params)
+    @charity.user = current_user
+    if @charity.save!
+      redirect_to my_profile_path, notice: "Welcome #{@charity.name}. You will be contacted by admin to verify your account."
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    @charity.update(charity_params)
+    redirect_to my_profile_path, notice: "#{@charity.name} account has been updated."
   end
 
   def destroy
     @charity.destroy
+    redirect_to root_path, notice: "#{@charity.name} account has been deleted."
   end
 
   private
 
   def set_charity
     @charity = Charity.find(params[:id])
+  end
+
+  def charity_params
+    params.require(:charity).permit(:name, :bio, :address, :email, :website, :phone_number, :photo)
   end
 end
