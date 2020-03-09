628d18bc9e5128a19f3395e332dcf72e77bb923a
Katie King <katharinemargaretking@gmail.com>
2020-03-10 09:35:28 +1100

    halfway

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'hell@foodforfree.online'
+  default from: 'hello@foodforfree.online'
   layout 'mailer'
 end
--- test/mailers/previews/user_business_mailer_preview.rb
+++ test/mailers/previews/user_business_mailer_preview.rb
@@ -3,7 +3,9 @@ class UserBusinessMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/user_business_mailer/welcome
   def welcome
-    UserBusinessMailer.welcome
+    user = BusinessUser.first
+
+    UserBusinessMailer.with(user: user).welcome.deliver_now
   end
 
 end
