3b9e3345578b11f46a34f68f39c0dfb28031056b
Katie King <katharinemargaretking@gmail.com>
2020-03-10 14:56:23 +1100

    charity email

---
+++ app/mailers/user_charity_mailer.rb
@@ -0,0 +1,13 @@
+class UserCharityMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_charity_mailer.welcome.subject
+  #
+    def welcome
+    @user = params[:user]
+    @claim = params[:claim]
+    mail(to: @user.email, subject: 'Food For Free - You have claimed a donation!')
+  end
+end
